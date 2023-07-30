00dc1b077ed25e17d0d36a1a31567990d8cb2107
Teresa Vidal <teresa.vidal2001@alumnos.ubiobio.cl>
2023-07-30 16:48:01 -0400

    vista citas

--- frontend/src/app/Agendar/page.js
+++ frontend/src/app/Agendar/page.js
@@ -1,23 +1,38 @@
+"use client";
 import React from "react";
-import { AgendarList } from "./agendarList";
-import Link from "next/link";
+import { Box, Link, Button } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
+import AgendarList from "./agendarList";
 
-const Button = () => {
+const CreateButton = () => {
   return (
-    <Link href="/Agendar/new">
-      <p className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-        Crear cita
-      </p>
-    </Link>
+    <Flex justifyContent="center" alignItems="center" height="100vh" mt="-20%">
+      <Link href="/Agendar/new">
+        <Button
+          colorScheme="teal"
+          size="sm"
+          borderRadius="md"
+          px={3}
+          py={2}
+          fontSize="sm"
+          fontWeight="medium"
+          _hover={{ bg: "teal.600" }}
+          _focus={{ boxShadow: "outline" }}
+        >
+          Crear cita
+        </Button>
+      </Link>
+    </Flex>
   );
 };
 
 const Agendar = () => {
   return (
-    <section>
-      <Button />
+    <Box>
       <AgendarList />
-    </section>
+      <CreateButton />
+    </Box>
   );
 };
+
 export default Agendar;
